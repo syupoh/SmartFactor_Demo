98fd39d6f6f7261fba6497136ae45de8a1c0f685
syupoh <5879dhtkdduq>
2020-02-18 01:14:45 +0900

    commit

--- Demo_UI_200117.py
+++ Demo_UI_200117.py
@@ -19,10 +19,6 @@ class App:
         self.butsz = 200
         patch_size = 128
 
-        # open video source (by default this will try to open the computer webcam)
-        # self.video_source = video_source
-        # self.vid = MyVideoCapture(self.video_source)
-
         self.window = window12
         self.window.title(window_title)
         self.screen_w = self.window.winfo_screenwidth()
@@ -35,8 +31,7 @@ class App:
         # self.win_x = int(self.screen_w/10)
         # self.win_y = int(self.screen_h/11)
 
-
-        main_img_w = int(self.win_w-30)
+        main_img_w = int(self.win_w-70)
         main_img_h = int(self.win_h/2)
         main_img_x = int((self.win_w - main_img_w)/2)
         main_img_y = int(10)
@@ -72,7 +67,6 @@ class App:
         detected_img4_tk = PIL.ImageTk.PhotoImage(detected_img4)
         detected_img5_tk = PIL.ImageTk.PhotoImage(detected_img5)
         
-        
         label1 = tkinter.Label(self.window, text="defect 갯수 {0}".format(n_defect), height=5)
         label2 = tkinter.Label(self.window, text="defect 크기 {0}".format(size_defect), height=5)
         label3 = tkinter.Label(self.window, text="defect 간 거리 {0}".format(dist_defect), height=5)
@@ -85,23 +79,17 @@ class App:
         detected_lbl5 = tkinter.Label(self.window, image=detected_img5_tk)
 
         main_lbl.place(x=main_img_x, y=main_img_y, width=main_img_w, height=main_img_h)
-        detected_lbl1.place(x=10, y=patch_y, width=patch_size, height=patch_size)
-        detected_lbl2.place(x=10+(patch_size+10)*1, y=patch_y, width=patch_size, height=patch_size)
-        detected_lbl3.place(x=10+(patch_size+10)*2, y=patch_y, width=patch_size, height=patch_size)
-        detected_lbl4.place(x=10+(patch_size+10)*3, y=patch_y, width=patch_size, height=patch_size)
-        detected_lbl5.place(x=10+(patch_size+10)*4, y=patch_y, width=patch_size, height=patch_size)
-        label1.place(x=10+(patch_size+10)*5+10, y=patch_y, height=13)
-        label2.place(x=10+(patch_size+10)*5+10, y=patch_y+(13+5)*1, height=13)
-        label3.place(x=10+(patch_size+10)*5+10, y=patch_y+(13+5)*2, height=13)
+        detected_lbl1.place(x=main_img_x, y=patch_y, width=patch_size, height=patch_size)
+        detected_lbl2.place(x=main_img_x+(patch_size+10)*1, y=patch_y, width=patch_size, height=patch_size)
+        detected_lbl3.place(x=main_img_x+(patch_size+10)*2, y=patch_y, width=patch_size, height=patch_size)
+        detected_lbl4.place(x=main_img_x+(patch_size+10)*3, y=patch_y, width=patch_size, height=patch_size)
+        detected_lbl5.place(x=main_img_x+(patch_size+10)*4, y=patch_y, width=patch_size, height=patch_size)
+        label1.place(x=main_img_x+(patch_size+10)*5+10, y=patch_y, height=13)
+        label2.place(x=main_img_x+(patch_size+10)*5+10, y=patch_y+(13+5)*1, height=13)
+        label3.place(x=main_img_x+(patch_size+10)*5+10, y=patch_y+(13+5)*2, height=13)
 
         ##########################################
-        
-
         # Button that lets the user take a snapshot
-        # self.btn_snapshot = tkinter.Button(self.window, overrelief="solid",
-        #                                    text="Detect", width=int(self.win_w/10), command=self.btn_snapshot)
-        # self.btn_snapshot.place(x=30, y=int(self.win_h/2), width=int(self.win_w/10))
-
 
         # After it is called once, the update method will be automatically called every delay mill
         self.delay = 20
