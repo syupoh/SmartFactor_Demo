c00504c9fda416fcc7d8ca21647c19e6f19e0a45
syupoh <5879dhtkdduq>
2020-02-18 11:16:41 +0900

    commit

--- Demo_UI_200117_2.py
+++ Demo_UI_200117_2.py
@@ -25,12 +25,12 @@ class App:
 
         self.screen_w = self.window.winfo_screenwidth()
         self.screen_h = self.window.winfo_screenheight()
-        self.win_w = int(self.screen_w/6*5)
-        self.win_h = int(self.screen_h/2)
+        self.win_w = int(self.screen_w-150)
+        self.win_h = int(self.screen_h-130)
         self.win_x = int(80)
         self.win_y = int(30)
 
-        self.main_img_w = int(self.win_w-30)
+        self.main_img_w = int((self.win_w-30)/3)
         self.main_img_h = int(self.win_h/2)
 
         self.window.geometry("{0}x{1}+{2}+{3}".format(self.win_w, self.win_h, self.win_x, self.win_y))
@@ -41,7 +41,9 @@ class App:
         self.size_defect = "5 2 3 6 2"
         self.dist_defect = "2 3 1 5 6"
 
-        self.main_img = PIL.Image.open("stitched-leather-texture.jpg")
+        self.main_img1 = PIL.Image.open("stitched-leather-texture.jpg")
+        self.main_img2 = PIL.Image.open("stitched-leather-texture.jpg")
+        self.main_img3 = PIL.Image.open("stitched-leather-texture.jpg")
         detected_img1 = PIL.Image.open("stitched-leather-texture_1.png")
         detected_img2 = PIL.Image.open("stitched-leather-texture_2.jpg")
         detected_img3 = PIL.Image.open("stitched-leather-texture_3.jpg")
@@ -50,9 +52,17 @@ class App:
         
         ##########################################
 
-        self.main_img = self.main_img.resize((self.main_img_w, self.main_img_h))
-        self.main_img_tk = PIL.ImageTk.PhotoImage(self.main_img)
-        self.main_lbl = tkinter.Label(self.window, image=self.main_img_tk)
+        self.main_img1 = self.main_img1.resize((self.main_img_w, self.main_img_h))
+        self.main_img1_tk = PIL.ImageTk.PhotoImage(self.main_img1)
+        self.main_lbl1 = tkinter.Label(self.window, image=self.main_img1_tk)
+
+        self.main_img2 = self.main_img2.resize((self.main_img_w, self.main_img_h))
+        self.main_img2_tk = PIL.ImageTk.PhotoImage(self.main_img2)
+        self.main_lbl2 = tkinter.Label(self.window, image=self.main_img2_tk)
+        
+        self.main_img3 = self.main_img3.resize((self.main_img_w, self.main_img_h))
+        self.main_img3_tk = PIL.ImageTk.PhotoImage(self.main_img3)
+        self.main_lbl3 = tkinter.Label(self.window, image=self.main_img3_tk)
 
         detected_img1 = detected_img1.resize((self.patch_size, self.patch_size))
         detected_img2 = detected_img2.resize((self.patch_size, self.patch_size))
@@ -77,33 +87,42 @@ class App:
         self.label3 = tkinter.Label(self.window, text="defect 간 거리 {0}".format(self.dist_defect), height=5)
 
         ##########################################
-        # Button that lets the user take a snapshot
-
+        
         # After it is called once, the update method will be automatically called every delay mill
-        self.delay = 100
+        self.delay = 300
         self.update()
 
         self.window.mainloop()
 
-        
+
 
     def update(self):
-
         self.win_w = self.window.winfo_width()
         self.win_h = self.window.winfo_height()
 
-        self.main_img_w = int(self.win_w-30)
+        self.main_img_w = int((self.win_w-30)/3)
         self.main_img_h = int(self.win_h/2)
-        self.main_img_x = int((self.win_w - self.main_img_w)/2)
+        self.main_img_x = int((self.win_w - self.main_img_w*3 - 20)/2)
         self.main_img_y = int(10)
         self.patch_y = int(self.win_h - (self.patch_size + 10))  
 
         if (self.main_img_w > 0 and self.main_img_h >0):
-            self.main_img = self.main_img.resize((self.main_img_w, self.main_img_h))
-            self.main_img_tk = PIL.ImageTk.PhotoImage(self.main_img)
-            self.main_lbl = tkinter.Label(self.window, image=self.main_img_tk)
+            self.main_img1 = self.main_img1.resize((self.main_img_w, self.main_img_h))
+            self.main_img1_tk = PIL.ImageTk.PhotoImage(self.main_img1)
+            self.main_lbl1 = tkinter.Label(self.window, image=self.main_img1_tk)
 
-        self.main_lbl.place(x=self.main_img_x, y=self.main_img_y, width=self.main_img_w, height=self.main_img_h)
+            self.main_img2 = self.main_img2.resize((self.main_img_w, self.main_img_h))
+            self.main_img2_tk = PIL.ImageTk.PhotoImage(self.main_img2)
+            self.main_lbl2 = tkinter.Label(self.window, image=self.main_img2_tk)
+            
+            self.main_img3 = self.main_img3.resize((self.main_img_w, self.main_img_h))
+            self.main_img3_tk = PIL.ImageTk.PhotoImage(self.main_img3)
+            self.main_lbl3 = tkinter.Label(self.window, image=self.main_img3_tk)
+            
+
+        self.main_lbl1.place(x=self.main_img_x, y=self.main_img_y, width=self.main_img_w, height=self.main_img_h)
+        self.main_lbl2.place(x=self.main_img_x + (self.main_img_w + 10) * 1, y=self.main_img_y, width=self.main_img_w, height=self.main_img_h)
+        self.main_lbl3.place(x=self.main_img_x + (self.main_img_w + 10) * 2, y=self.main_img_y, width=self.main_img_w, height=self.main_img_h)
         self.detected_lbl1.place(x=self.main_img_x, y=self.patch_y, width=self.patch_size, height=self.patch_size)
         self.detected_lbl2.place(x=self.main_img_x+(self.patch_size+10)*1, y=self.patch_y, width=self.patch_size, height=self.patch_size)
         self.detected_lbl3.place(x=self.main_img_x+(self.patch_size+10)*2, y=self.patch_y, width=self.patch_size, height=self.patch_size)
@@ -114,6 +133,7 @@ class App:
         self.label1.place(x=self.main_img_x+(self.patch_size+10)*5+10, y=self.patch_y, height=13)
         self.label2.place(x=self.main_img_x+(self.patch_size+10)*5+10, y=self.patch_y+(13+5)*1, height=13)
         self.label3.place(x=self.main_img_x+(self.patch_size+10)*5+10, y=self.patch_y+(13+5)*2, height=13)
+        
         self.window.after(self.delay, self.update)
 
 
